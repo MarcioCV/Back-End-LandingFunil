6ebc1583ffcd4d93f1a45eb67ab75e6868e3e915
Danizinh <danimaltoc@outlook.com>
2022-09-18 23:04:24 -0300

    changing the database

--- process/conn.php
+++ process/conn.php
@@ -5,17 +5,8 @@
   $pass = "Funil_ronald";
   $db = "";
   $host = "localhost";
-  try {
 
-    $conn = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-  } catch (PDOException $e) {
-
-    print "Erro: " . $e->getMessage() . "<br/>";
-    die();
-
-}
-
+  $mysqli = new $mysqli($host,$user,$password,$db);
+  if($mysqli->error)
+  die("Falha ao conectar ao banco de dados:" . $mysqli->error);
 ?>
