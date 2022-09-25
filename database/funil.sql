d2a416faa7e82dff726f035f30fc451871128cab
Danizinh <danimaltoc@outlook.com>
2022-09-24 21:04:27 -0300

    update database

--- database/funil.sql
+++ database/funil.sql
@@ -1,14 +1,18 @@
 CREATE DATABASE Funil_ronald;
 
 USE Funil_ronald;
+CREATE TABLE
+    User(
+        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        name VARCHAR(100),
+        phone VARCHAR(20),
+        email VARCHAR(225),
+        password VARCHAR(50),
+        profile_pictures VARCHAR(500),
+        whatsapp_group VARCHAR(500),
+        telegrama_group VARCHAR(500),
+        binance_link VARCHAR(500),
+        link_royalQ VARCHAR(300)
+    );
 
-CREATE TABLE User(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL ,
-    name VARCHAR(100),
-    phone VARCHAR(20),
-    email VARCHAR(225),
-    password VARCHAR(50),
-    profile_pictures VARCHAR(500),
-    whatsapp_group VARCHAR(500),
-    telegrama_group VARCHAR(500)
-);
+DROP DATABASE  Funil_ronald;
