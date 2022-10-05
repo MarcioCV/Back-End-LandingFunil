f5de30f7fec83b5e3425ae7150236b4cb1be1056
Danizinh <danimaltoc@outlook.com>
2022-10-04 23:48:31 -0300

    today's update

--- html/reset_password.php
+++ html/reset_password.php
@@ -1,4 +1,18 @@
+<?php echo"http://" . $_SERVER["HTTP_HOST"] . dirname($_SERVER["PHP_SELF"]) . "/reset_password.php?code";?>
+
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="../php/requestReset.php">FORGOT PASSWORD?</a>
+</body>
+</html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -63,4 +77,4 @@
     <script src="../js/script.js"></script>
 </body>
 
-</html>
+</html> -->
--- php/requestReset.php
+++ php/requestReset.php
@@ -1,7 +1,5 @@
 <?php
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -22,24 +20,25 @@ if(isset($_POST ["email"])){
 
     $mail = new PHPMailer(true);
     try {
-        //Server settings
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'danimaltoc@gmail.com';                     //SMTP username
-        $mail->Password   = 'oinnqhzicddzpxyk';                               //SMTP password
-        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->isSMTP();                                            
+        $mail->Host       = 'smtp.gmail.com';                    
+        $mail->SMTPAuth   = true;                                   
+        $mail->Username   = 'danimaltoc@gmail.com';                   
+        $mail->Password   = 'oinnqhzicddzpxyk';                               
+        $mail->SMTPSecure = 'tls';            
+        $mail->Port       = 587;                                   
     
-        //Recipients
+       
         $mail->setFrom('danimaltoc@gmail.com', 'RendaInfinita');
         $mail->addAddress("$emailTo");     //Add a recipient
         $mail->addReplyTo('no-reply@gmail.com', 'No reply');
     
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = "This is the HTML message body <b>in bold!</b> $code";
+    
+        $url = "http://" . $_SERVER["HTTP_HOST"] . dirname($_SERVER["PHP_SELF"]);
+        $mail->isHTML(true);                                 
+        $mail->Subject = 'Your password reset link';
+        $mail->Body    = "<h1> You requested a password reset </h1>;
+                            Click this link  to do so";
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     
         $mail->send();
@@ -47,9 +46,8 @@ if(isset($_POST ["email"])){
     }catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
-    exit();
+    exit(); // Sem a possibilidade de solicitar outra solicitacao de  reset password
 }
-
 ?>
 
 <!DOCTYPE html>
